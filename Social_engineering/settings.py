202ae33c0c46ef0d0b595fa70924ede63dc834bf
Herry <59681685+Cheng-Yen1230@users.noreply.github.com>
2020-11-21 14:32:02 +0800

    socialengineering1.0

--- Social_engineering/settings.py
+++ Social_engineering/settings.py
@@ -73,18 +73,16 @@ WSGI_APPLICATION = 'Social_engineering.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
+import dj_database_url
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    'default': dj_database_url.config()
 
-        # 'NAME': 'mydb2',
-        # 'USER': 'postgres',
-        # 'PASSWORD': '0000',
-        # 'PORT': '5432',
-        # 'HOST': 'localhost'
-    }
+    # 'NAME': 'mydb2',
+    # 'USER': 'postgres',
+    # 'PASSWORD': '0000',
+    # 'PORT': '5432',
+    # 'HOST': 'localhost'
 }
 
 # Password validation
